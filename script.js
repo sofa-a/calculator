d43d3dd0c64a17f0865d79bdd3870233cf598e2c
sofa <matassa.sophia@gmail.com>
2024-11-18 12:31:36 +0800

    almost completed the button functionality

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-function add() {
-    
-}
-
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
+function add() { return num1 + num2; }
+function subtract() { return num1 - num2 }
+function multiply(num1, num2) { return num1 * num2; }
+function divide(num1, num2) { return num1 / num2;}
 
 function operate(operator, num1, num2) {
+    let result;
     case(operator) {
         case "+":
-            add(num1, num2);
+            result = add(num1, num2);
             break;
         case "-":
-            subtract(num1, num2);
+            result = subtract(num1, num2);
             break;
         case "*":
-            multiply(num1, num2);
+            result = multiply(num1, num2);
             break;
         case "/":
-            divide(num1, num2);
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
+
+function handleClick(e, array) {
+    let element = e.target.getAttribute("id");
+    case (element) {
+        case "clear":
+            break;
+        case "=":
+            break;
+        default:
+            const div = document.createElement("div");
+            div.textContent = element;
+            div
             break;
     }
 }
+
+function main() {
+    const array = [];
+    const buttons = document.querySelectorAll("button");
+    for (const button of buttons) {
+        button.addEventListener("click", (e) => handleClick(e, array));
+    }
+    
+}
+
+main();
